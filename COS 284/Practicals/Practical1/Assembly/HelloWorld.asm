95ff37664b28a4f85d8a00095e0fecea88e0601a
ReganKoopmans <u15043143@tuks.co.za>
2016-07-22 06:27:20 +0200

    Added assembly,fortran to prac1

---
+++ COS 284/Practicals/Practical1/Assembly/HelloWorld.asm
@@ -0,0 +1,22 @@
+; Program:  Print "Hello world!"
+; Input:    None
+; Output:   "Hello world!"
+; Author:   Regan Koopmans
+
+  section .data
+    msg dw  "Hello world!",0xa,0xd
+    len equ $-msg
+
+  section .text
+    global _start
+
+_start:
+  mov eax,4
+  mov ebx,1
+  mov ecx,msg
+  mov edx,len
+  int 0x80
+
+  mov eax,1
+  mov ebx,0
+  int 0x80
