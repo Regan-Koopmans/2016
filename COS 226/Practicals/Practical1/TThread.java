4a50ff51d08efa2a98297b59afdc74848ae8b31c
= <regankoopmans@yahoo.com>
2016-07-25 15:49:27 +0200

    Added some 226

--- COS 226/Practicals/Practical1/TThread.java
+++ COS 226/Practicals/Practical1/TThread.java
@@ -1,8 +1,13 @@
 //Practical assignment 1
 //Thread created by extending the Thread class
 
-class TThread extends Thread {
-	public void run() {
-		System.out.println("Thread is running...");
+class TThread extends Thread 
+{
+	int getValue;
+	public void run() 
+	{
+		Thread.sleep(400);
+		getValue = getAndIncrement();
+		System.out.println(Thread.getName() + " " + getValue);
 	}
 }
